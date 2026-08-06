fa8ff5d598a99d5d17303091c2caa470d94bbe19
FerPeraldo-ui <fernando.peraldoa@gmail.com>
2026-08-05 22:04:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ valor_final = st.number_input("Ingrese el valor final")
 lista_numeros = list(range(int(valor_inicial), int(valor_final)))
 st.write(lista_numeros)
 
-modulos = st.sidebar.sleectbox ("Selecione un módulo",["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
+modulos = st.sidebar.selectbox ("Selecione un módulo",["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
 
 
