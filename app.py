53f24a768dded54d06c3a720ad5f6d61bf85aab3
FerPeraldo-ui <fernando.peraldoa@gmail.com>
2026-08-05 22:01:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,16 @@ st.write("Elaborado por Fernando Peraldo")
 
 valor_inicial = st.number_input("Ingrese el valor inicial")
 valor_final = st.number_input("Ingrese el valor final")
+
+lista_numeros = list(range(int(valor_inicial), int(valor_final)))
+st.write(lista_numeros)
+
+modulos = st.sidebar.sleectbox ("Selecione un módulo",["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
+
+if modulo == "Módulo Listas": 
+  st.write("Bienvenido al módulo de Listas")
+elif modulo == "Módulo Arreglos": 
+  st.write("Bienvenido al módulo de Arreglos")
+else: 
+  st.write("Bienvenido al módulo de Funciones")
+  
