a2a5b6677761a7064cab858b54b5504a596b7004
FerPeraldo-ui <fernando.peraldoa@gmail.com>
2026-08-05 21:17:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 import streamlit as st
+
+st.title("Especialización Python for Analytics")
+st.sedebar.title("Parámetros")
+st.write("Elaborado por Fernando Peraldo")
+
+valor_inicial = st.number_input("Ingrese el valor inicial")
+valor_final = st.number_input("Ingrese el valor final")
